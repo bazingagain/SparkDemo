1bc42a00ac27c889abf38ecc7e69116627acaffe
bazingagain <1152204424@qq.com>
2018-04-21 15:01:30 +0800

    first commit

---
+++ FirstSpark/src/main/java/com/leon/learnspark/SparkApp.java
@@ -0,0 +1,38 @@
+package com.leon.learnspark;
+
+import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.api.java.JavaSparkContext;
+import org.apache.spark.api.java.function.Function;
+
+/**
+ * Created on 2018/4/20.
+ *
+ * @author Xiaolei-Peng
+ */
+public class SparkApp {
+
+    public static void main(String[] args) {
+        String logFile = "FirstSpark/data/test.txt"; // Should be some file on your system
+        SparkConf conf = new SparkConf().setAppName("Simple Application");
+        JavaSparkContext sc = new JavaSparkContext(conf);
+        JavaRDD<String> logData = sc.textFile(logFile).cache();
+
+        long numAs = logData.filter(new Function<String, Boolean>() {
+            public Boolean call(String s) {
+                return s.contains("a");
+            }
+        }).count();
+
+        long numBs = logData.filter(new Function<String, Boolean>() {
+            public Boolean call(String s) {
+                return s.contains("b");
+            }
+        }).count();
+
+        System.out.println("Lines with a: " + numAs + ", lines with b: " + numBs);
+
+        sc.stop();
+    }
+
+}
